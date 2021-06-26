e713b3df41dc20c6fb7c701f486cc9d209533a35
오형민 <hyungmin.oh@nhn.com>
2021-06-27 00:21:31 +0900

    apply maven to project and add lombok dependency

--- src/main/java/design_pattern/singleton_pattern/simple_singleton.java
+++ src/main/java/design_pattern/singleton_pattern/simple_singleton.java
@@ -1,8 +1,9 @@
 package design_pattern.singleton_pattern;
 
+
 public class simple_singleton {
     public static void main(String[] args) {
-
+        System.out.println("hello world");
     }
 
 }
